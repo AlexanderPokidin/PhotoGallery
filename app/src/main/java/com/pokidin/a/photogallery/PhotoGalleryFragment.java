58758b340509f71df3735d0f56945c8cce146b82
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-06 11:37:23 +0300

    RecyclerView added

---
+++ app/src/main/java/com/pokidin/a/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,41 @@
+package com.pokidin.a.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mPhotoRecyclerView;
+
+//    TODO: check method
+    public static PhotoGalleryFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        PhotoGalleryFragment fragment = new PhotoGalleryFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+}
